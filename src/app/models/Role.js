67e032dc4a0d6e12239a78dda383c50bbed23aae
DiogodePaula <depaulatrabalhos@gmail.com>
2020-09-07 01:13:42 -0300

    ok

--- src/app/models/Role.js
+++ src/app/models/Role.js
@@ -1,46 +1,47 @@
-import Sequelize, {
-    Model,
-} from 'sequelize';
+import Sequelize, { Model } from 'sequelize';
 
 class Role extends Model {
-    static init(sequelize) {
-        super.init({
-            uid: {
-                primaryKey: true,
-                allowNull: false,
-                type: Sequelize.UUID,
-                defaultValue: Sequelize.UUIDV4,
-            },
-            name: {
-                allowNull: false,
-                type: Sequelize.STRING,
-            },
-            company_uid: {
-                type: Sequelize.UUID,
-                allowNull: false,
-                references: {
-                    model: 'companies',
-                    key: 'uid',
-                },
-                onUpdate: 'CASCADE',
-                onDelete: 'CASCADE',
-            }
-        }, {
-            sequelize,
-        });
-        return this;
-    }
+  static init(sequelize) {
+    super.init(
+      {
+        uid: {
+          primaryKey: true,
+          allowNull: false,
+          type: Sequelize.UUID,
+          defaultValue: Sequelize.UUIDV4,
+        },
+        name: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        company_uid: {
+          type: Sequelize.UUID,
+          allowNull: false,
+          references: {
+            model: 'companies',
+            key: 'uid',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
 
-    static associate(models) {
-        this.belongsTo(models.Company, {
-            as: 'company',
-            foreignKey: 'company_uid',
-        });
-        this.hasMany(models.EmployeeRole, {
-            as: 'employees',
-            foreignKey: 'role_uid',
-        });
-    }
+  static associate(models) {
+    this.belongsTo(models.Company, {
+      as: 'company',
+      foreignKey: 'company_uid',
+    });
+    this.hasMany(models.EmployeeRole, {
+      as: 'employees',
+      foreignKey: 'role_uid',
+    });
+  }
 }
 
 export default Role;
